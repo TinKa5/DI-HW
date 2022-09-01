66394ee2b3084262bcf90ffb7a6c35ee0ec9d72d
vs <vs@playa.tech>
2022-09-02 01:50:44 +0300

    change server IP

--- lect_02/ht_template/job1/main.py
+++ lect_02/ht_template/job1/main.py
@@ -43,4 +43,4 @@ def main() -> flask_typing.ResponseReturnValue:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8081)
+    app.run(debug=True, host="localhost", port=8081)
